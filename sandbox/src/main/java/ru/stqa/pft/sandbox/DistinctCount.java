f71a017173e1f747bfdf74ea5a6d61ad3ecdf7f2
Elena130387 <89135144+Elena130387@users.noreply.github.com>
2021-08-24 00:59:27 +0300

    Перенос метода расчета расстояния в класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistinctCount.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistinctCount.java
@@ -2,12 +2,9 @@ package ru.stqa.pft.sandbox;
 
 public class DistinctCount {
   public static void main(String[] args) {
-    Point p1 = new Point(-8, 5);
-    Point p2 = new Point(10, 7);
-    System.out.println("Расстояние между точками " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y + " = " + distance(p1, p2));
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) - (p2.y - p1.y) * (p2.y - p1.y));
+    Point p1 = new Point(-10, 5, 10,7);
+    Point p2 = new Point(-8, 3, 10,5);
+    System.out.println("Расстояние между точками " + p1.x1 + ";" + p1.y1 + " и " + p1.x2 + ";" + p1.y2 + " = " + p1.distance());
+    System.out.println("Расстояние между точками " + p2.x1 + ";" + p2.y1 + " и " + p2.x2 + ";" + p2.y2 + " = " + p2.distance());
   }
 }
