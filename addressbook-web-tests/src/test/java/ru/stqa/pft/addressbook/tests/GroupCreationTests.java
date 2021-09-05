e7d216559e972e2f13b4145d53122701e1997f7f
Elena130387 <89135144+Elena130387@users.noreply.github.com>
2021-09-06 00:16:37 +0300

    Добавила обработку дефолтных значений в класс HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test_new", "test_new1", "test_new2"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test_new", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
     app.logout();
