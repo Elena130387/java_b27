ed97ff17177abaca304034f9d68b92bf0b82d039
Elena130387 <89135144+Elena130387@users.noreply.github.com>
2021-09-16 00:55:38 +0300

    Добавила сравнение списков в методы удаления и модификации групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -16,11 +17,16 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> befor = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(befor.size() - 2);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test_new4", "test_new2", "test_new2"));
+    GroupData group = new GroupData( befor.get(befor.size() - 2).getId(), "test_new5", "test_new2", "test_new2");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), befor.size());
+
+    befor.remove(befor.size() - 2);
+    befor.add(group);
+    Assert.assertEquals( new HashSet<Object>(after), new HashSet<Object>(befor));
     app.logout();
   }
 }
