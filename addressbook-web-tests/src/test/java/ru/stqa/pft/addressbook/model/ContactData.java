1a383c01f054f0887109a42ec562d28872559625
Elena130387 <89135144+Elena130387@users.noreply.github.com>
2021-09-17 01:22:58 +0300

    Модернизировала метод создания контактов и сравнение списков в нем

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class ContactData {
-  private final String id;
+  private int id;
   private final String firstname;
   private final String lastname;
   private final String address;
@@ -12,7 +12,7 @@ public class ContactData {
   private String group;
 
 
-  public ContactData(String id, String firstname, String lastname, String address, String mobile, String email, String group) {
+  public ContactData(int id, String firstname, String lastname, String address, String mobile, String email, String group) {
     this.id = id;
     this.firstname = firstname;
     this.lastname = lastname;
@@ -23,13 +23,16 @@ public class ContactData {
   }
 
   public ContactData(String firstname, String lastname, String address, String mobile, String email, String group) {
-    this.id = null;
+    this.id = 0;
     this.firstname = firstname;
     this.lastname = lastname;
     this.address = address;
     this.mobile = mobile;
     this.email = email;
     this.group = group;
+  }
+  public int getId() {
+    return id;
   }
 
   public String getFirstname() {
@@ -57,19 +60,6 @@ public class ContactData {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    ContactData that = (ContactData) o;
-    return Objects.equals(id, that.id) && Objects.equals(firstname, that.firstname) && Objects.equals(lastname, that.lastname);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, firstname, lastname);
-  }
-
-  @Override
   public String toString() {
     return "ContactData{" +
             "id='" + id + '\'' +
@@ -81,8 +71,16 @@ public class ContactData {
             ", group='" + group + '\'' +
             '}';
   }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ContactData that = (ContactData) o;
+    return id == that.id && Objects.equals(firstname, that.firstname) && Objects.equals(lastname, that.lastname);
+  }
 
-  public String getId() {
-    return id;
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, firstname, lastname);
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,7 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -11,9 +13,20 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().goToHome();
     List<ContactData> befor = app.getContactHelper().getContactList();
-    app.getContactHelper().createContact(new ContactData("ALEX", "Shapoval", "Spb, Verbnaya st, h.1", "89554050801", "8888@rambler.ru", "test_new"), true);
+    ContactData contact = new ContactData("TEST3", "Shapoval", "Spb, Verbnaya st, h.4", "89554050801", "8888@rambler.ru", null);
+    app.getContactHelper().createContact(contact, true);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), befor.size() + 1);
+
+    int max = 0;
+    for (ContactData с: after){
+      if (с.getId() > max) {
+        max = с.getId();
+      }
+    }
+    //contact.setId(max);
+    befor.add(contact);
+    Assert.assertEquals( new HashSet<Object>(after), new HashSet<Object>(befor));
     app.logout();
   }
 }
