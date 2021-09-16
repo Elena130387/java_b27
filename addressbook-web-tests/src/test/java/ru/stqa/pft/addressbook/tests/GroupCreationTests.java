ed2769c3322f7bf1437bb9e70fe441c04ea21d17
Elena130387 <89135144+Elena130387@users.noreply.github.com>
2021-09-17 01:16:16 +0300

    Добавила в метод создания контактов сравнение двух списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase{
@@ -12,10 +13,21 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> befor = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test_new", null, null));
+    GroupData group = new GroupData("test_new", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), befor.size() + 1);
+
+    int max = 0;
+    for (GroupData g: after){
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    befor.add(group);
+    Assert.assertEquals( new HashSet<Object>(after), new HashSet<Object>(befor));
+
     app.logout();
   }
-
 }
