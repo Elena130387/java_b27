fd64a12f1130fe6dd79c8cd258290f6f0ff75c55
Elena130387 <89135144+Elena130387@users.noreply.github.com>
2021-10-15 13:36:02 +0300

    8.4 Ошибка:No tests found for given includes:

--- mantis-tests/src/test/java/ru/stqa/pft/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/appmanager/ApplicationManager.java
@@ -1,6 +1,6 @@
 package ru.stqa.pft.appmanager;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,10 +12,12 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+
 public class ApplicationManager {
   private final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
   private String browser;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser)  {
     this.browser = browser;
@@ -26,23 +28,41 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-    wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
   }
 
-  public void logout() {
-    wd.findElement(By.linkText("Logout")).click();
-  }
+  //public void logout() {
+  //  wd.findElement(By.linkText("Logout")).click();
+  //}
 
   public void stop() {
-    wd.quit();
+    if (wd != null){
+      wd.quit();
+    }
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null){
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null){
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
   }
 }
