5641f28aa64dcacb517778017c0959c95ce596d6
Elena130387 <89135144+Elena130387@users.noreply.github.com>
2021-10-17 18:24:02 +0300

    8.6 Не работает нажатие на кнопку "Изменить пользователя" при завершении регистрации

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -21,8 +21,8 @@ public class RegistrationHelper extends HelperBase{
     type(By.name("realname"), user);
     type(By.name("password"), password);
     type(By.name("password_confirm"), password);
-    click(By.cssSelector("input[value='submit']"));
-    //click(By.cssSelector("input[value='Update User']"));
+    //click(By.cssSelector("input[value='submit']"));
+    click(By.cssSelector("input[value='Update User']"));
     //click(By.cssSelector("input[value='Изменить пользователя']"));
   }
 }
